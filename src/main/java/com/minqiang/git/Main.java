1c94d54bde0cbe00ffabcbe79ff5062e3af37733
guodegangdaye <guodegangdaye@126.com>
2022-12-14 02:40:32 +0800

    github test

---
+++ src/main/java/com/minqiang/git/Main.java
@@ -0,0 +1,11 @@
+package com.minqiang.git;
+
+/**
+ * @Author Minqiang
+ * @Date ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
